485d1f45b526bf302499324b99645c70087f3a93
Olga Surnina <veehja88@gmail.com>
2023-11-02 15:36:32 +0300

    task done

--- src/main/java/Army.java
+++ src/main/java/Army.java
@@ -3,15 +3,12 @@ public class Army {
     public static void main(String[] args) {
 
         int armyLannister = 147483648;
-
         System.out.println("Пехота Ланнистеров насчитывает: " + armyLannister + " единиц");
 
         int armySaruman = 1147483848;
-
         System.out.println("Пехота Сарумана насчитывает: " + armySaruman + " единиц");
 
         int sumOfArmies = (armyLannister + armySaruman);
-
         System.out.println("Общее число войск: " + sumOfArmies + " единиц");
     }
 }
