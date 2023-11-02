aaba7288d8be221566e6280f025e3d3b0f5c74a3
Olga Surnina <veehja88@gmail.com>
2023-11-02 09:43:13 +0300

    Initial commit

---
+++ src/main/java/Army.java
@@ -0,0 +1,11 @@
+public class Army {
+
+    public static void main(String[] args) {
+
+        int armyLannister = 147483648;
+        int armySaruman = 1147483848;
+        int sumOfArmies = (armyLannister + armySaruman);
+
+        System.out.println("Общее число войск: " + sumOfArmies + " единиц");
+    }
+}
